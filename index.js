4f9ef331b699c7b6540edf2a2dec6f024800a499
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-15 15:11:34 -0600

    318.1.1 LAB | Part 2: Node Command Line Interface - DONE

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Part 1: Node Installation | Node has been installed previously.
+
+// Part 2: Node Command Line Interface.
+        // Node -v, npm -v, node -e and node -p have been tested on the terminal
+        // index.js file has been created. and console.log has been added to the file
+
+        console.log ("hello World!");
+    
