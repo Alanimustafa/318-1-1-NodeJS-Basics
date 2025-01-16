658dcbce67a7f8a9a4c5eb32f67983331d9a22a5
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-15 18:32:25 -0600

    Part 4: Creating a Server

--- index.js
+++ index.js
@@ -4,11 +4,26 @@
         // Node -v, npm -v, node -e and node -p have been tested on the terminal
         // index.js file has been created. and console.log has been added to the file
 
-        console.log ("hello World!");
+            //    console.log ("hello World!");
 
 // Part 3: Node Package Manager - Installed 
         // npm has been itailized in this project.
         // npm start command has been added to the package.json file.
         // nodemon has been installed.
-        
-    
+
+// Part 4: Creating a Server
+    const http = require ('http'); // http variable module type (http) has been created using "require" keyword
+    const hostname = "127.0.0.1"; // Defining the local address.
+    const port = 3000; // defining the port 
+
+    // crating a server for the hpp module using createServer method. 
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200 ;
+        res.setHeader('content-type', 'text/plain');
+        res.end('Hello World!\n');
+    });
+
+    // Using the listen method to make the http server listen to the communications using the port and the hostname.
+    server.listen(port, hostname ,() => {
+        console.log(`Server running at http://${hostname}:${port}`);
+    });
